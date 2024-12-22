ec04538b6733e170f4fa00a93153963be798b749
KamilCiepluch <kamil.ciepluch@wp.pl>
2024-12-22 20:44:13 +0100

    Fixed create_mask.py imports
    It fixes typo in create_mask.py imports

--- create_mask.py
+++ create_mask.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
     from torchmetrics.functional.image import structural_similarity_index_measure  # NOQA
     import numpy as np
     from tqdm import tqdm
-    from VGG16_new import VGG16Autoencoder
+    from VGG16_inpating import VGG16Autoencoder
 
 
 
